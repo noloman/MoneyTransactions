9f9710c601df772014e10308298ba1798baee5a0
Manuel Lorenzo <manulorenzop@gmail.com>
2020-01-07 13:27:32 +0100

    remove remanents of presentation and repository modules after merging into master branch

--- libraries/repository/src/main/java/me/manulorenzo/moneytransactions/repository/Repository.kt
+++
@@ -1,6 +0,0 @@
-package me.manulorenzo.moneytransactions.repository
-
-interface Repository {
-    suspend fun getAccount(): me.manulorenzo.moneytransactions.data_account.AccountData?
-    suspend fun getTransactionString(fileName: String = "transactions.json"): String
-}
